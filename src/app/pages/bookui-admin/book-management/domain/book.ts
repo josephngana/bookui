f541214dfcc921c30eeb45b049227cf374ed7580
caniksea <caniksea@yahoo.co.nz>
2018-09-21 10:33:32 +0200

    Adding a siteId member to the Book domain

--- src/app/pages/bookui-admin/book-management/domain/book.ts
+++ src/app/pages/bookui-admin/book-management/domain/book.ts
@@ -2,6 +2,7 @@ import {BookBase} from './book-base';
 import {Chapter} from './chapter';
 
 export class Book extends BookBase {
+  siteId: String;
   isbn: String;
   eisbn: String;
   author: String;
