ac7755412540cacf9888e6bc15c06bd79ceadd4e
keanu <kmlduncan@gmail.com>
2018-09-21 13:18:31 +0200

    Added Site Management UI

--- src/app/pages/bookui-admin/site-management/sites/sites.component.ts
+++ src/app/pages/bookui-admin/site-management/sites/sites.component.ts
@@ -3,9 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-sites',
   templateUrl: './sites.component.html',
-  styleUrls: ['./sites.component.scss']
+  styleUrls: ['./sites.component.scss'],
 })
 export class SitesComponent implements OnInit {
+
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title: 'ID',
+        type: 'number',
+      },
+      siteName: {
+        title: 'Site Name',
+        type: 'string',
+      },
+      siteDescription: {
+        title: 'Site Description',
+        type: 'string',
+      },
+    },
+  };
 
   constructor() { }
 
