e67315720e2166e6c5b2dffbb01df96d67650752
NicoleJestine <abrahamsnicole1@gmail.com>
2018-09-25 12:24:05 +0200

    Sections UI added

--- src/app/pages/bookui-admin/book-management/sections/sections.component.ts
+++ src/app/pages/bookui-admin/book-management/sections/sections.component.ts
@@ -7,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionsComponent implements OnInit {
 
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      section: {
+        title: 'Section',
+        type: 'string',
+      },
+      author: {
+        title: 'Author',
+        type: 'string',
+      },
+      date: {
+        title: 'Date',
+        type: 'date',
+      },
+    },
+  };
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/bookui-admin/book-management/sections/sections.module.ts
+++ src/app/pages/bookui-admin/book-management/sections/sections.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionsComponent } from './sections.component';
 import {ThemeModule} from '../../../../@theme/theme.module';
+import {Ng2SmartTableModule} from 'ng2-smart-table';
 
 const components = [
   SectionsComponent,
@@ -11,6 +12,7 @@ const components = [
   imports: [
     CommonModule,
     ThemeModule,
+    Ng2SmartTableModule,
   ],
   declarations: [...components],
   exports: [...components],
