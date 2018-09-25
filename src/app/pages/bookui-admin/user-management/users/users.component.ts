c664181ade94d99facf0ac1772c55bad0a8072b8
glorykas <glorykasongo07@gmail.com>
2018-09-25 13:11:20 +0200

    The user UI table is done

--- src/app/pages/bookui-admin/user-management/users/users.component.ts
+++ src/app/pages/bookui-admin/user-management/users/users.component.ts
@@ -8,6 +8,59 @@ import {User} from '../domain/user';
 })
 export class UsersComponent implements OnInit {
 
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      siteName: {
+        title: 'Site Name',
+        type: '',
+      },
+      firstName: {
+        title: 'First Name',
+        filter: {
+          type: 'list',
+          config: {
+            selectText: 'Select...',
+            list: [
+              { value: 'Glenna Reichert', title: 'Glenna Reichert' },
+              { value: 'Kurtis Weissnat', title: 'Kurtis Weissnat' },
+              { value: 'Chelsey Dietrich', title: 'Chelsey Dietrich' },
+            ],
+          },
+        },
+      },
+      lastName: {
+        title: 'Last Name',
+        type: 'string',
+      },
+      username: {
+        title: 'Username',
+        type: 'string',
+      },
+      email: {
+        title: 'E-mail',
+        type: 'string',
+      },
+      password: {
+        title: 'Password',
+        type: 'password',
+      },
+    },
+  };
+
   constructor() {
     this.doShow();
   }
