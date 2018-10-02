c865b96c7b24a99a809cdaecb6025d678ff7f6f7
glorykas <glorykasongo07@gmail.com>
2018-10-02 10:57:34 +0200

    subsections modals added

---
+++ src/app/pages/bookui-admin/book-management/modals/add-edit-subsection/add-edit-subsection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-add-edit-subsection',
+  templateUrl: './add-edit-subsection.component.html',
+  styleUrls: ['./add-edit-subsection.component.scss']
+})
+export class AddEditSubsectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/bookui-admin/book-management/modals/add-edit-subsection/add-edit-subsection.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddEditSubsectionComponent } from './add-edit-subsection.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AddEditSubsectionComponent]
+})
+export class AddEditSubsectionModule { }
